d5537bc0e5d2b4cc4f6f64fabbc521d29efdcc32
Michael J. Deale <mdeale@syncurity.net>
2017-05-17 12:19:59 -0400

    Fix sample scripts for latest version of library

--- examples/02_csv_to_alerts.py
+++ examples/02_csv_to_alerts.py
@@ -6,7 +6,7 @@ from time import gmtime, strftime
 
 pp = pprint.PrettyPrinter(indent = 4)
 
-irflowAPI = irflow_client.IRFlowClient("./api.conf")
+irflowAPI = irflow_client.IRFlowClient(config_file="./api.conf")
 if irflowAPI.debug == "true":
 	irflowAPI.dump_settings()
 
@@ -18,7 +18,7 @@ with open('sample_csv_of_alerts.csv', 'rb') as csv_file:
 	for row in csv_reader:
 		result = irflowAPI.create_alert(row, description = description, incoming_field_group_name = alert_type)
 		if result['success']:
-			print 'Created Alert_Num: ' + str(result['data']['alert_num'])
+			print 'Created Alert_Num: ' + str(result['data']['alert']['alert_num'])
 		else:
 			print 'Failed to create alert'
 			print 'Success == ' + str(result['success'])
