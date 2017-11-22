4d697f35fabd984ea6271b4eef16938e8ff0f6d9
punkrokk <jp.bourget@gmail.com>
2017-11-22 12:10:16 -0500

    move conf.py napolean stuff around

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,45 +35,6 @@ except ImportError:
     print('Cannot import example documentation auto-generation functionality')
     can_auto_generate = False
 
-# -- Extensions to the  Napoleon GoogleDocstring class ---------------------
-
-# Title: Extensions to the Napoleon GoogleDocstring class
-# Author: Michael Goerz
-# Date: 2015.12.30
-# Code Version: N/A
-# Availability: https://michaelgoerz.net/notes/extending-sphinx-napoleon-docstring-sections.html
-
-
-# first, we define new methods for any new sections and add them to the class
-def parse_keys_section(self, section):
-    return self._format_fields('Keys', self._consume_fields())
-
-
-def parse_attributes_section(self, section):
-    return self._format_fields('Attributes', self._consume_fields())
-
-
-def parse_class_attributes_section(self, section):
-    return self._format_fields('Class Attributes', self._consume_fields())
-
-
-GoogleDocstring._parse_keys_section = parse_keys_section
-GoogleDocstring._parse_attributes_section = parse_attributes_section
-GoogleDocstring._parse_class_attributes_section = parse_class_attributes_section
-
-
-# we now patch the parse method to guarantee that the the above methods are
-# assigned to the _section dict
-def patched_parse(self):
-    self._sections['keys'] = self._parse_keys_section
-    self._sections['class attributes'] = self._parse_class_attributes_section
-    self._unpatched_parse()
-
-
-GoogleDocstring._unpatched_parse = GoogleDocstring._parse
-GoogleDocstring._parse = patched_parse
-
-
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
@@ -250,26 +211,30 @@ if can_auto_generate:
 
 # -- Extensions to the  Napoleon GoogleDocstring class ---------------------
 
+# Title: Extensions to the Napoleon GoogleDocstring class
+# Author: Michael Goerz
+# Date: 2015.12.30
+# Code Version: N/A
+# Availability: https://michaelgoerz.net/notes/extending-sphinx-napoleon-docstring-sections.html
+
+
 # first, we define new methods for any new sections and add them to the class
 def parse_keys_section(self, section):
     return self._format_fields('Keys', self._consume_fields())
-
-
-GoogleDocstring._parse_keys_section = parse_keys_section
 
 
 def parse_attributes_section(self, section):
     return self._format_fields('Attributes', self._consume_fields())
 
 
-GoogleDocstring._parse_attributes_section = parse_attributes_section
-
-
 def parse_class_attributes_section(self, section):
     return self._format_fields('Class Attributes', self._consume_fields())
 
 
+GoogleDocstring._parse_keys_section = parse_keys_section
+GoogleDocstring._parse_attributes_section = parse_attributes_section
 GoogleDocstring._parse_class_attributes_section = parse_class_attributes_section
+
 
 # we now patch the parse method to guarantee that the the above methods are
 # assigned to the _section dict
@@ -277,5 +242,7 @@ def patched_parse(self):
     self._sections['keys'] = self._parse_keys_section
     self._sections['class attributes'] = self._parse_class_attributes_section
     self._unpatched_parse()
+
+
 GoogleDocstring._unpatched_parse = GoogleDocstring._parse
 GoogleDocstring._parse = patched_parse
