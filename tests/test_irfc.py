3c912a0907af7fa09a7f4c378a5cf6859b8a81e6
punkrokk <jp.bourget@gmail.com>
2017-11-29 13:04:59 -0500

    first testing commit.

--- tests/test_irfc.py
+++ tests/test_irfc.py
@@ -1,104 +1,13 @@
-import json
+"""
+    test_irfc.py. Pytests for irflow_client
+
+    You must have test_data.py present in the tests directory to run tests successfully
+"""
 import pytest
 
 from irflow_client import IRFlowClient
 
-config_args = {
-    'address': '50.19.169.130',
-    'api_user': 'api',
-    'api_key': '7ac832c355b1e6a840caaf9b6f96dc462fe003cc',
-    'debug': True,
-    'protocol': 'https'
-}
-
-config_args_missing_parameters = [
-    {   # No API User key['api_user']
-        'address': '50.19.169.130',
-        'api_key': '7ac832c355b1e6a840caaf9b6f96dc462fe003cc',
-        'debug': True,
-        'protocol': 'https'
-    }, {   # No API Key key['api_key']
-        'address': '50.19.169.130',
-        'api_user': 'api',
-        'debug': True,
-        'protocol': 'https'
-    }, {   # No API Server key['address']
-        'api_user': 'api',
-        'api_key': '7ac832c355b1e6a840caaf9b6f96dc462fe003cc',
-        'debug': True,
-        'protocol': 'https'
-    },
-]
-
-config_args_to_try = [
-    {  # api_user = None
-        'address': '50.19.169.130',
-        'api_user': None,
-        'api_key': '7ac832c355b1e6a840caaf9b6f96dc462fe003cc',
-        'debug': True,
-        'protocol': 'https'
-    }, {  # api_user = int
-        'address': '50.19.169.130',
-        'api_user': 123,
-        'api_key': '7ac832c355b1e6a840caaf9b6f96dc462fe003cc',
-        'debug': True,
-        'protocol': 'https'
-    }, {  # api_user = list
-        'address': '50.19.169.130',
-        'api_user': ['abc', '123'],
-        'api_key': '7ac832c355b1e6a840caaf9b6f96dc462fe003cc',
-        'debug': True,
-        'protocol': 'https'
-    }, {  # api_user = Bool
-        'address': '50.19.169.130',
-        'api_user': True,
-        'api_key': '7ac832c355b1e6a840caaf9b6f96dc462fe003cc',
-        'debug': True,
-        'protocol': 'https'
-    },{  # api_key = None
-        'address': '50.19.169.130',
-        'api_user': 'api',
-        'api_key': None,
-        'debug': True,
-        'protocol': 'https'
-    }, {  # api_key = int
-        'address': '50.19.169.130',
-        'api_user': 'api',
-        'api_key': 1,
-        'debug': True,
-        'protocol': 'https'
-    },{  # api_key = list
-        'address': '50.19.169.130',
-        'api_user': 'api',
-        'api_key': ['123', 'abc'],
-        'debug': True,
-        'protocol': 'https'
-    }, {  # api_key = Bool
-        'address': '50.19.169.130',
-        'api_user': 'api',
-        'api_key': True,
-        'debug': True,
-        'protocol': 'https'
-    }
-]
-
-proofpoint_message = {
-    "fields":
-        {
-            "emailTo": ["test@example.com"],
-            "emailDate": "2017-07-21T01:06:05.000Z",
-            "emailFrom": "=?utf-8?B?VS5TIEZlZGVyYWwgRmluYW5jaWFsIERlYnQgRGVwYXJ0bWVudA==?= ",
-            "sourceAddress": "31.207.80.114", "emailSubject": "Collection of real estate",
-            "deviceExternalId": "496568c0d9e7d8e8f9f74946a5e62dbde42944c27601ebcae26856ac646766a1",
-            "originalAlertURL": "https:\/\/threatinsight.proofpoint.com\/302f9667-0366-df19-1c34-d02dd8ac4ab9\/threat\/email\/496568c0d9e7d8e8f9f74946a5e62dbde42944c27601ebcae26856ac646766a1",
-            "category": "malware",
-            "threatScores": [44, 0, 0]
-        },
-    "suppress_missing_field_warning": False,
-    "description": "PyTest Proofpoint Messages Delivered",
-    "data_field_group_name": "Proofpoint Messages Delivered"
-}
-
+import test_data
 
 # Test Client Setup Error handling
 # def test_irfc_no_username():
@@ -109,6 +18,13 @@ proofpoint_message = {
 #         IRFlowClient(config_args=config_args)
 #     exception_msg = excinfo.value.args[0]
 #     assert exception_msg == 'api_user'
+
+# Test data
+
+config_args = test_data.config_args
+config_args_missing_parameters = test_data.config_args_missing_parameters
+config_args_to_try = test_data.config_args_to_try
+proofpoint_message = test_data.proofpoint_message
 
 
 @pytest.fixture(params=config_args_to_try)
@@ -142,7 +58,11 @@ def test_irfc_init(config_args_generator):
 
 @pytest.fixture(autouse=True)
 def irfc():
-    # Setup : start IRFlow Client
+    """
+    Setup irflow_client instance
+    :return:
+        IRFlowClient
+    """
 
     return IRFlowClient(config_args)
 
